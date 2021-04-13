3345ff61956f893438d426782aa06fb38fe57fc8
Chereq <mr.dead.snyper@gmail.com>
2021-04-13 20:18:31 +0300

    main.py update

--- main.py
+++ main.py
@@ -2,90 +2,182 @@
 # -*- coding: utf-8 -*-
 
 import pygame as pg
+from pygame import sprite
 from pprint import pprint
 
 
-WIN_WIDTH = 32*11
-WIN_HEIGHT = 32*11
+# for field
+BLOCK_WIDTH = 64
+BLOCK_HEIGHT = 64
+WIN_WIDTH = BLOCK_WIDTH * 13
+WIN_HEIGHT = BLOCK_HEIGHT * 15
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
-BACKGROUND_COLOR = "#388700"
-PLATFORM_WIDTH = 32
-PLATFORM_HEIGHT = 32
-PLATFORM_COLOR = "#b0b0b0"
+BACKGROUND_COLOR = "#101010"#"#388700"
+BLOCK_COLOR = "#b0b0b0"
+
+# for player
+MOVE_SPEED = BLOCK_WIDTH // 8
+WIDTH = BLOCK_WIDTH
+HEIGHT = BLOCK_HEIGHT
+COLOR =  "#888888"
 
 # 29 * 13
 
-class Block(pg.sprite.Sprite):
-    def __init__(self, x, y, undercover=None):
-        pg.sprite.Sprite.__init__(self)
-        # super().__init__()
-        self.undercover = undercover
-        self.image = pg.Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.image.fill(pg.Color(PLATFORM_COLOR))
-        self.rect = pg.Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+def get_closer_center(x, y):
+    return round(x / BLOCK_WIDTH) * BLOCK_WIDTH, round(y / BLOCK_HEIGHT) * BLOCK_HEIGHT
 
-    def place(self, x, y):
-        pass
+class Block(sprite.Sprite):
+    def __init__(self, x, y, sprites=None, sprite_tile=None):
+        sprite.Sprite.__init__(self)
+        # super().__init__()
+        self.image = pg.Surface((BLOCK_WIDTH, BLOCK_HEIGHT))
+        self.image.fill(pg.Color(BLOCK_COLOR))
+        self.rect = pg.Rect(x, y, BLOCK_WIDTH, BLOCK_HEIGHT)
 
     def __repr__(self):
         return "O"
 
 class WallBlock(Block):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.image = ss.image_at((BLOCK_WIDTH * 3,
+        #                             BLOCK_HEIGHT * 3,
+        #                             BLOCK_WIDTH,
+        #                             BLOCK_HEIGHT))
+
     def __repr__(self):
-        return "X"
+        return f"X{self.rect.x // BLOCK_WIDTH, self.rect.y // BLOCK_HEIGHT}"
 
 class BrickBlock(Block):
     def __repr__(self):
-        return "#"
+        return f"#{self.rect.x // BLOCK_WIDTH, self.rect.y // BLOCK_HEIGHT}"
+
+class Bomb(Block):
+    pass
+        
 
 
-MOVE_SPEED = 7
-WIDTH = 22
-HEIGHT = 32
-COLOR =  "#888888"
-class Player(pg.sprite.Sprite):
-    def __init__(self, x, y):
-        pg.sprite.Sprite.__init__(self)
-        self.xvel = 0   #скорость перемещения. 0 - стоять на месте
-        self.startX = x # Начальная позиция Х, пригодится когда будем переигрывать уровень
-        self.startY = y
-        self.image = pg.Surface((WIDTH, HEIGHT))
-        self.image.fill(pg.Color(COLOR))
-        self.rect = pg.Rect(x, y, WIDTH, HEIGHT) # прямоугольный объект
+class Player(sprite.Sprite):
+    def __init__(self, x, y, image=None):
+        sprite.Sprite.__init__(self)
+        self.xvel = self.yvel = 0
+        if image is None:
+            self.image = pg.Surface((WIDTH, HEIGHT))
+            self.image.fill(pg.Color(COLOR))
+        elif isinstance(image, pg.Surface):
+            self.image = image
+        else:
+            pass
+        self.rect = pg.Rect(x, y, WIDTH, HEIGHT)
 
     def update(self, horizontal, vertical):
         self.xvel = horizontal
         self.yvel = vertical
 
-        self.rect.x += self.xvel
-        self.rect.y += self.yvel
+        self.rect.x += self.xvel * MOVE_SPEED
+        self.rect.y += self.yvel * MOVE_SPEED
 
-    # def draw(self, screen): # Выводим себя на экран
-    #     screen.blit(self.image, (self.rect.x,self.rect.y))
+    def update(self, horizontal, vertical, action, blocks):
+        # if not vertical:
+        self.xvel = horizontal
+        self.yvel = vertical
 
-    def collide(self, xvel, yvel, blocks):
-        for block in blocks:
-            if pg.sprite.collide_rect(self, block):
-                if xvel > 0:
-                    self.rect.right = p.rect.left # то не движется вправо
+        # if self.xvel > 0:
+        #     self.image.fill(pg.Color(COLOR))
+        #     self.bolt_anim_right.blit(self.image, (0, 0))
+        # elif self.xvel < 0:
+        #     self.image.fill(pg.Color(COLOR))
+        #     self.bolt_anim_left.blit(self.image, (0, 0))
+        # elif self.yvel > 0:
+        #     self.image.fill(pg.Color(COLOR))
+        #     self.bolt_anim_up.blit(self.image, (0, 0))
+        # elif self.yvel < 0:
+        #     self.image.fill(pg.Color(COLOR))
+        #     self.bolt_anim_down.blit(self.image, (0, 0))
 
-                if xvel < 0:                      # если движется влево
-                    self.rect.left = p.rect.right # то не движется влево
+        self.rect.y += self.yvel * MOVE_SPEED
+        self.rect.x += self.xvel * MOVE_SPEED
+        self.collide(blocks)
 
-                if yvel > 0:                      # если падает вниз
-                    self.rect.bottom = p.rect.top # то не падает вниз
-                    self.onGround = True          # и становится на что-то твердое
-                    self.yvel = 0                 # и энергия падения пропадает
+        if action:
+            x, y = get_closer_center(self.rect.x, self.rect.y)
+            bomb = Bomb(x, y)
+            return bomb
 
-                if yvel < 0:                      # если движется вверх
-                    self.rect.top = p.rect.bottom # то не движется вверх
-                    self.yvel = 0                 # и энергия прыжка пропадает
 
+    def collide(self, blocks):
+        collide_list = sprite.spritecollide(self, blocks, False)
+
+        move_h = move_v = 0
+        for collision in collide_list:
+            if collision.rect.x < self.rect.x:
+                move_h += 1
+
+            if collision.rect.x > self.rect.x:
+                move_h -= 1
+
+            if collision.rect.y < self.rect.y:
+                move_v += 1
+
+            if collision.rect.y > self.rect.y:
+                move_v -= 1
+        
+        if move_h > 0:
+            self.rect.x += MOVE_SPEED
+        elif move_h < 0:
+            self.rect.x -= MOVE_SPEED
+        if move_v > 0:
+            self.rect.y += MOVE_SPEED
+        elif move_v < 0:
+            self.rect.y -= MOVE_SPEED
+
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x,self.rect.y))
+
+
+class SpriteSheet:
+    def __init__(self, filename):
+        try:
+            self.sheet = pg.image.load(filename).convert()
+        except pg.error as message:
+            print('Unable to load spritesheet image:', filename)
+            raise SystemExit(message)
+
+    def image_at(self, rectangle, colorkey=None):
+        "Loads image from x, y, x + offset, y + offset"
+        rect = pg.Rect(rectangle)
+        image = pg.Surface(rect.size).convert()
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is None:
+            colorkey = image.get_at((0, 0))
+        elif isinstance(colorkey, pg.Color):
+            image.set_colorkey(colorkey, pg.RLEACCEL)
+        return image
+
+    def images_at(self, rects, colorkey=None):
+        "Loads multiple images, supply a list of coordinates"
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    def load_strip(self, rect, image_count, colorkey=None):
+        "Loads a strip of images and returns them as a list"
+        tups = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
+
+    def load_table(self, rect, rows, cols, colorkey=None):
+        "Loads cols*rows sprites"
+        ret = []
+        for i in range(rows):
+            tups = [(rect[0] + rect[2] * x, rect[1] + rect[3] * i, rect[2], rect[3])
+                for x in range(cols)]
+            ret.append(self.images_at(tups, colorkey))
+        return ret
 
 
 demo_field = [
     [["#"]] * 11,
-    [["#"]] + [[]] * 9 + [["#"]],
+    [["#"]] + [["B"]] * 9 + [["#"]],
     [["#"], []] * 5 + [["#"]],
     [["#"]] + [[]] * 9 + [["#"]],
     [["#"], []] * 5 + [["#"]],
@@ -105,25 +197,33 @@ def main():
     pg.display.set_caption("Bomberman")
     bg = pg.Surface((WIN_WIDTH, WIN_HEIGHT))
 
+    ss = SpriteSheet('./media/sprites_hq.png')
+    sprite_images = ss.load_table((0, 0, BLOCK_WIDTH, BLOCK_HEIGHT), 14, 12)
+
     bg.fill(pg.Color(BACKGROUND_COLOR))
 
-    sprites_group = pg.sprite.Group()
-    x = y = 0
+    sprites_group = sprite.Group()
+    blocks = []
+    x = y = BLOCK_WIDTH
     for row in demo_field:
         for cell in row:
             if cell:
-                block = WallBlock(x, y)
-                if cell[0] == '#':
-                    pass
+                block = WallBlock(x, y, sprites=None)
+                if cell[0] == 'B':
+                    block = BrickBlock(x, y, sprites=None)
                 sprites_group.add(block)
-            x += PLATFORM_WIDTH
-        y += PLATFORM_HEIGHT
-        x = 0
-
-    player = Player(PLATFORM_WIDTH * 5, PLATFORM_HEIGHT * 5)
+                blocks.append(block)
+            x += BLOCK_WIDTH
+        y += BLOCK_HEIGHT
+        x = BLOCK_WIDTH
+    player = Player(BLOCK_WIDTH * 6, BLOCK_HEIGHT * 5, image=ss.image_at((BLOCK_WIDTH * 4,
+                                                                                   BLOCK_HEIGHT * 0,
+                                                                                   BLOCK_WIDTH,
+                                                                                   BLOCK_HEIGHT)))
     sprites_group.add(player)
 
     horizontal = vertical = 0
+    action = False
 
     while True:
         for event in pg.event.get():
@@ -140,6 +240,8 @@ def main():
                     vertical = -1
                 if event.key == pg.K_DOWN:
                     vertical = 1
+                if event.key == pg.K_SPACE:
+                    action = True
             if event.type == pg.KEYUP:
                 if event.key == pg.K_LEFT or event.key == pg.K_RIGHT:
                     horizontal = 0
@@ -147,9 +249,22 @@ def main():
                     vertical = 0
 
 
-        player.update(horizontal, vertical)
+        ret = player.update(horizontal, vertical, action, blocks)
+        if ret:
+            sprites_group.add(ret)
+            blocks.append(ret)
+
+        action = False
+
+        player.draw(screen)
         screen.blit(bg, (0, 0))
         sprites_group.draw(screen)
+
+        # for i, strip in enumerate(sprite_images):
+        #     for j, image in enumerate(strip):
+        #         screen.blit(image, (j*BLOCK_WIDTH, i*BLOCK_HEIGHT))
+
+        # screen.blit(sprite_images[0][5], (0, 0))
 
         pg.display.update()
         timer.tick(30)
