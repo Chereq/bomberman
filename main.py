da91c90a052667ed8f5fefc268c6e609c32cae89
Chereq <mr.dead.snyper@gmail.com>
2021-04-13 23:42:30 +0300

    bombs and explosios prototyped

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ BLOCK_HEIGHT = 64
 WIN_WIDTH = BLOCK_WIDTH * 13
 WIN_HEIGHT = BLOCK_HEIGHT * 15
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
-BACKGROUND_COLOR = "#101010"#"#388700"
+BACKGROUND_COLOR = "#388700"  #"#101010"
 BLOCK_COLOR = "#b0b0b0"
 
 # for player
@@ -21,6 +21,7 @@ MOVE_SPEED = BLOCK_WIDTH // 8
 WIDTH = BLOCK_WIDTH
 HEIGHT = BLOCK_HEIGHT
 COLOR =  "#888888"
+ANIMATION_RATE = 10
 
 # 29 * 13
 
@@ -28,7 +29,7 @@ def get_closer_center(x, y):
     return round(x / BLOCK_WIDTH) * BLOCK_WIDTH, round(y / BLOCK_HEIGHT) * BLOCK_HEIGHT
 
 class Block(sprite.Sprite):
-    def __init__(self, x, y, sprites=None, sprites_tile=None):
+    def __init__(self, x, y, sprites_tile=None):
         # sprite.Sprite.__init__(self)
         super().__init__()
         self.image = pg.Surface((BLOCK_WIDTH, BLOCK_HEIGHT))
@@ -38,11 +39,13 @@ class Block(sprite.Sprite):
     def __repr__(self):
         return "O"
 
+    def update(self, time):
+        super().update()
+
 class WallBlock(Block):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if "sprites_tile" in kwargs:
-            self.image = kwargs["sprites_tile"][3][3]
+        self.image = kwargs["sprites_tile"][3][3]
 
     def __repr__(self):
         return f"X{self.rect.x // BLOCK_WIDTH, self.rect.y // BLOCK_HEIGHT}"
@@ -50,69 +53,87 @@ class WallBlock(Block):
 class BrickBlock(Block):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if "sprites_tile" in kwargs:
-            self.image = kwargs["sprites_tile"][3][4]
+        self.image = kwargs["sprites_tile"][3][4]
+        self.anim_die = kwargs["sprites_tile"][3][5:11]
 
     def __repr__(self):
         return f"#{self.rect.x // BLOCK_WIDTH, self.rect.y // BLOCK_HEIGHT}"
 
 class Bomb(Block):
-    pass
-        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.image = kwargs["sprites_tile"][3][0]
+        self.countdown = 3
+        self.animation_rate = ANIMATION_RATE / (self.countdown + .5)
+        self.animation_timeout = 0
+        self.anim_static = cycle(kwargs["sprites_tile"][3][0:3] + kwargs["sprites_tile"][3][2:-1:-1])
+        self.anim_die = kwargs["sprites_tile"][3][5:11]
+
+    def update(self, time):
+        self.animation_timeout += time
+        self.countdown -= time / 1000
+        self.animation_rate = ANIMATION_RATE / (self.countdown + .5)
+        if self.animation_timeout / 1000 >= 1 / self.animation_rate:
+            self.animation_timeout = 0
+            self.image = next(self.anim_static)
+
+    def is_exploded(self):
+        return self.countdown <= 0
+
+    def get_epicenter(self):
+        return self.rect.x, self.rect.y
 
 
-class Player(sprite.Sprite):
+class Explosion(Block):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.radius = 1
+        self.blast_speed = 5
+        self.ray_power = 1
+
+    def update(self, time):
+        pass
+
+
+class Actor(sprite.Sprite):
     def __init__(self, x, y, sprites_tile=None):
-        sprite.Sprite.__init__(self)
         self.xvel = self.yvel = 0
         self.anim_right = self.anim_left = self.anim_up = self.anim_down = self.anim_die = self.static_image = None
-        if sprites_tile is None:
-            self.image = pg.Surface((WIDTH, HEIGHT))
-            self.image.fill(pg.Color(COLOR))
-        else:
+        self.image = pg.Surface((WIDTH, HEIGHT))
+        self.image.fill(pg.Color(COLOR))
+        self.rect = pg.Rect(x, y, WIDTH, HEIGHT)
+        self.animation_timeout = 0
+
+
+class Player(Actor):
+    def __init__(self, x, y, sprites_tile=None):
+        super().__init__(x, y)
+        if sprites_tile:
             self.image = self.static_image = sprites_tile[0][4]
             self.anim_right = cycle(sprites_tile[1][0:3])
             self.anim_left = cycle(sprites_tile[0][0:3])
             self.anim_up = cycle(sprites_tile[1][3:6])
             self.anim_down = cycle(sprites_tile[0][3:6])
             self.anim_die = cycle(sprites_tile[2][:7])
-        self.rect = pg.Rect(x, y, WIDTH, HEIGHT)
 
-    def update(self, horizontal, vertical):
+    def update(self, horizontal, vertical, action, blocks, time):
+
         self.xvel = horizontal
         self.yvel = vertical
+        self.animation_timeout += time
 
-        self.rect.x += self.xvel * MOVE_SPEED
-        self.rect.y += self.yvel * MOVE_SPEED
-
-    def update(self, horizontal, vertical, action, blocks):
-        # if not vertical:
-        self.xvel = horizontal
-        self.yvel = vertical
-
-        if horizontal > 0:
-            self.image = next(self.anim_right)
-        elif horizontal < 0:
-            self.image = next(self.anim_left)
-        elif vertical > 0:
-            self.image = next(self.anim_down)
-        elif vertical < 0:
-            self.image = next(self.anim_up)
-        else:
-            self.image = self.static_image
-
-        # if self.xvel > 0:
-        #     self.image.fill(pg.Color(COLOR))
-        #     self.bolt_anim_right.blit(self.image, (0, 0))
-        # elif self.xvel < 0:
-        #     self.image.fill(pg.Color(COLOR))
-        #     self.bolt_anim_left.blit(self.image, (0, 0))
-        # elif self.yvel > 0:
-        #     self.image.fill(pg.Color(COLOR))
-        #     self.bolt_anim_up.blit(self.image, (0, 0))
-        # elif self.yvel < 0:
-        #     self.image.fill(pg.Color(COLOR))
-        #     self.bolt_anim_down.blit(self.image, (0, 0))
+        if self.animation_timeout / 1000 >= 1 / ANIMATION_RATE:
+            self.animation_timeout = 0
+            if self.xvel > 0:
+                self.image = next(self.anim_right)
+            elif self.xvel < 0:
+                self.image = next(self.anim_left)
+            elif self.yvel > 0:
+                self.image = next(self.anim_down)
+            elif self.yvel < 0:
+                self.image = next(self.anim_up)
+            else:
+                self.image = self.static_image
 
         self.rect.y += self.yvel * MOVE_SPEED
         self.rect.x += self.xvel * MOVE_SPEED
@@ -120,27 +141,29 @@ class Player(sprite.Sprite):
 
         if action:
             x, y = get_closer_center(self.rect.x, self.rect.y)
-            bomb = Bomb(x, y)
-            return bomb
+            return x, y
 
 
-    def collide(self, blocks):
-        collide_list = sprite.spritecollide(self, blocks, False)
-
+    def collide(self, list_of_sprites_group):
+       
         move_h = move_v = 0
-        for collision in collide_list:
-            if collision.rect.x < self.rect.x:
-                move_h += 1
+       
+        for sprites_group in list_of_sprites_group:
+       
+            collide_list = sprite.spritecollide(self, sprites_group, False)
+            for collision in collide_list:
+                if collision.rect.x < self.rect.x:
+                    move_h += 1
 
-            if collision.rect.x > self.rect.x:
-                move_h -= 1
+                if collision.rect.x > self.rect.x:
+                    move_h -= 1
 
-            if collision.rect.y < self.rect.y:
-                move_v += 1
+                if collision.rect.y < self.rect.y:
+                    move_v += 1
 
-            if collision.rect.y > self.rect.y:
-                move_v -= 1
-        
+                if collision.rect.y > self.rect.y:
+                    move_v -= 1
+            
         if move_h > 0:
             self.rect.x += MOVE_SPEED
         elif move_h < 0:
@@ -222,6 +245,9 @@ def main():
     bg.fill(pg.Color(BACKGROUND_COLOR))
 
     sprites_group = sprite.Group()
+    bombs_group = sprite.Group()
+    explosions_group = sprite.Group()
+    actors_group = sprite.Group()
     # blocks = []
     x = y = BLOCK_WIDTH
     for row in demo_field:
@@ -242,6 +268,9 @@ def main():
     action = False
 
     while True:
+
+        milliseconds = timer.tick(30)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 raise SystemExit
@@ -265,15 +294,29 @@ def main():
                     vertical = 0
 
 
-        ret = player.update(horizontal, vertical, action, sprites_group)
-        sprites_group.update()
-        if ret:
-            sprites_group.add(ret)
+        ret = player.update(horizontal, vertical, action, (sprites_group, bombs_group, actors_group), time=milliseconds)
+        sprites_group.update(milliseconds)
+        actors_group.update(milliseconds)
+        bombs_group.update(milliseconds)
+        explosions_group.update(milliseconds)
+
+        if ret:# and not bombs_group:
+            bomb = Bomb(*ret, sprites_tile=sprites_tile)
+            bombs_group.add(bomb)
+
+        for bomb in bombs_group:
+            if bomb.is_exploded():
+                epicenter = bomb.get_epicenter()
+                bomb.kill()
+                explosion = Explosion(*epicenter)
+                explosions_group.add(explosion)
 
         action = False
 
         screen.blit(bg, (0, 0))
         sprites_group.draw(screen)
+        bombs_group.draw(screen)
+        explosions_group.draw(screen)
         player.draw(screen)
 
 
@@ -284,7 +327,6 @@ def main():
         screen.blit(sprites_tile[0][5], (0, 0))
 
         pg.display.update()
-        timer.tick(30)
         
 
 if __name__ == "__main__":
